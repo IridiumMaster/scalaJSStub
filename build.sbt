8b81f30b45c2a1153b569f209b68ae76036e4855
Travis Good <travis.godwin.good@gmail.com>
2014-02-14 22:38:19 -0600

    illustrates something that appears to work in .2 but not in .3

--- build.sbt
+++ build.sbt
@@ -5,10 +5,13 @@ name := "travisStub"
 
 version := "0.1-SNAPSHOT"
 
+// Replace the current dependency with this one to get a working test result. 
+//    "org.scala-lang.modules.scalajs" %% "scalajs-jasmine-test-framework" % scalaJSVersion % "test"
+
 libraryDependencies ++= Seq(
     "org.scala-lang.modules.scalajs" %% "scalajs-jquery" % "0.1-SNAPSHOT",
     "org.scala-lang.modules.scalajs" %% "scalajs-dom" % "0.1-SNAPSHOT",
-    "org.scala-lang.modules.scalajs" %% "scalajs-jasmine-test-framework" % "0.3-SNAPSHOT" % "test",
+    "org.scala-lang.modules.scalajs" %% "scalajs-jasmine-test-framework" % "0.3" % "test",
     "org.webjars" % "envjs" % "1.2" % "test"    
 )
 
--- src/test/scala/travisTest/travisstub/TravisstubTest.scala
+++ src/test/scala/travisTest/travisstub/TravisstubTest.scala
@@ -10,10 +10,31 @@ object TravisstubTest extends JasmineTest {
 
     it("should implement square()") {
       import Travisstub._
+      import scala.collection.immutable.Map
+      import scala.collection.immutable.Set
+
 
       expect(square(0)).toBe(0)
       expect(square(4)).toBe(16)
       expect(square(-5)).toBe(25)
+
+    }
+    
+      it("Should add an object to a map.") {
+      import Travisstub._
+      import scala.collection.immutable.Map
+      import scala.collection.immutable.Set
+
+
+      var topicConsumers = Map[String, Set[String]]()
+      
+      var theNewSet = Set[String]()
+      theNewSet += "testConsumer"
+      
+      topicConsumers += "testProducer" -> theNewSet
+
+      expect(topicConsumers("testProducer").contains("testConsumer")).toBe(true)
+      
     }
   }
 }
